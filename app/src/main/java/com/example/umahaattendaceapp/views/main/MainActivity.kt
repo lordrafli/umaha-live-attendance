d7cc15069ccaac7b1fdd46360af5f040258a644f
bayu <by.fajr@gmail.com>
2023-06-17 19:14:09 +0700

    ui: mainactivity for history,att,profile fragment

---
+++ app/src/main/java/com/example/umahaattendaceapp/views/main/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.umahaattendaceapp.views.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.umahaattendaceapp.R
+import com.example.umahaattendaceapp.databinding.ActivityMainBinding
+import com.example.umahaattendaceapp.views.attendance.AttendanceFragment
+import com.example.umahaattendaceapp.views.history.HistoryFragment
+import com.example.umahaattendaceapp.views.profile.ProfileFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        binding.btmNavigationMain.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.action_history -> {
+                    openFragment(HistoryFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.action_attendance -> {
+                    openFragment(AttendanceFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.action_profile -> {
+                    openFragment(ProfileFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
+        openHomeFragment()
+    }
+
+    private fun openHomeFragment() {
+        binding.btmNavigationMain.selectedItemId = R.id.action_attendance
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_main, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
