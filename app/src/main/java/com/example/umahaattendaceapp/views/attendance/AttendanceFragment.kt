a830e8cf11244df7984eb8351bf00782de31486f
bayu <by.fajr@gmail.com>
2023-06-21 01:10:43 +0700

    feat: attendance - permission chapter 1

--- app/src/main/java/com/example/umahaattendaceapp/views/attendance/AttendanceFragment.kt
+++ app/src/main/java/com/example/umahaattendaceapp/views/attendance/AttendanceFragment.kt
@@ -1,10 +1,13 @@
 package com.example.umahaattendaceapp.views.attendance
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.example.umahaattendaceapp.Manifest
 import com.example.umahaattendaceapp.R
 import com.example.umahaattendaceapp.databinding.FragmentAttendanceBinding
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -15,9 +18,24 @@ import com.google.android.gms.maps.model.LatLng
 
 class AttendanceFragment : Fragment(), OnMapReadyCallback {
 
+    companion object {
+        private const val REQUEST_CODE_MAP_PERMISSION = 1000
+    }
+        private val mapPermissions = arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+
+
+
+
+
     private var mapAttendance: SupportMapFragment? = null
     private var map: GoogleMap? = null
     private var binding: FragmentAttendanceBinding? = null
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,11 +57,33 @@ class AttendanceFragment : Fragment(), OnMapReadyCallback {
     }
 
 
-    override fun onMapReady(p0: GoogleMap) {
-        map = p0
-        val umaha = LatLng(-7.349545304399485, 112.68895525231115)
-        map?.moveCamera(CameraUpdateFactory.newLatLng(umaha))
-        map?.animateCamera(CameraUpdateFactory.zoomTo(20f))
+    private fun checkPermission(): Boolean {
+        var isHasPermission = false
+        context?.let {
+            for (permission in mapPermissions){
+                isHasPermission = ActivityCompat.checkSelfPermission(it, permission) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+        return isHasPermission
     }
 
+    override fun onMapReady(p0: GoogleMap) {
+        map = p0
+        if (checkPermission()) {
+            val umaha = LatLng(-7.349545304399485, 112.68895525231115)
+            map?.moveCamera(CameraUpdateFactory.newLatLng(umaha))
+            map?.animateCamera(CameraUpdateFactory.zoomTo(20f))
+            goToCurrentLocation()
+        } else {
+            setRequestPermission()
+        }
+    }
+
+    private fun setRequestPermission() {
+        TODO("Not yet implemented")
+    }
+
+    private fun goToCurrentLocation() {
+        TODO("Not yet implemented")
+    }
 }
