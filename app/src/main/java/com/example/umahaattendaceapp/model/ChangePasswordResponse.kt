e8e5062e952445c971dcbcd0f7f1bea076f99176
bayu <by.fajr@gmail.com>
2023-06-24 18:12:24 +0700

    feat: fitur change password

---
+++ app/src/main/java/com/example/umahaattendaceapp/model/ChangePasswordResponse.kt
@@ -0,0 +1,9 @@
+package com.example.umahaattendaceapp.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ChangePasswordResponse(
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
